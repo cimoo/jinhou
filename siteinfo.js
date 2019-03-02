e1e6461b1194e51bb316f160ee66360c6cc1df04
cimoo <18837275118@163.com>
2019-03-02 16:55:17 +0800

    不容易

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  acid: -1,
+  version: "1.0.0",
+  siteroot: "http://m.kinhome.cn",
+  apiroot: "https://m.kinhome.cn/web/index.php?store_id=1&r=api/"
+  //云硕提示  只需要改域名和storeid即可
+  //独立版：https://hj.weixin2015.cn/web/index.php?store_id=1&r=api/
+  //微擎版：https://www.weixin2015.cn/addons/zjhj_mall/core/web/index.php?store_id=1&r=api/
+};
